0a0b46310cd4c2bfb2a63eb341915990c45b1cb2
Mikkolla <kamenevkola05@gmail.com>
2023-09-26 20:48:35 +0300

    init repo

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import { Welcome } from "./components/Welcome";
+import { Soft } from "./components/Soft";
+import { Classroom } from "./components/Classroom";
+import { Features } from "./components/Features";
+import { News } from "./components/News";
+import { Footer } from "./components/footer";
+
+function App() {
+  return (
+    <>
+      <Welcome />
+      <Soft />
+      <Classroom />
+      <Features />
+      <News />
+      <Footer />
+    </>
+  );
+}
+
+export default App;
